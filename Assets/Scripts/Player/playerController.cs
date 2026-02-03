bc534b5585a45805a2c56b77c32e6878bbc12128
Patryk IMG2 <mikolajczyk.p.patryk@gmail.com>
2026-02-03 13:54:58 +0100

    Player + Alteruna multiplayer setup

---
+++ Assets/Scripts/Player/playerController.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public float horizontalInput = 1f;
+    public float verticalInput = 1f;
+    public float speed = 5f;
+
+    public float mouseSensitivity = 100f;
+    private float xRotation = 0f;
+    public Transform playerCamera;
+
+    private Rigidbody Rigid;
+
+    // -------------------------------------------------- 
+    private Alteruna.Avatar Avatar;
+    // -------------------------------------------------- 
+
+    void Start()
+    {
+        // -------------------------------------------------- 
+        Avatar = GetComponent<Alteruna.Avatar>();
+        if (!Avatar.IsMe)
+        {
+            Destroy(this);
+            return;
+        }
+        // -------------------------------------------------- 
+
+        Rigid = GetComponent<Rigidbody>();
+        Rigid.freezeRotation = true;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        transform.Rotate(Vector3.up * mouseX); // player rotate (yaw)
+
+        // camera (pitch)
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        playerCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+    }
+    void FixedUpdate()
+    {
+        Vector3 move = new Vector3(horizontalInput, 0f, verticalInput);
+        move = transform.TransformDirection(move);
+        transform.Translate(move * speed * Time.deltaTime, Space.World);
+    }
+}
