913a7452b97ed4a5aca54bca94c44c5ff82bf37b
Zeth <zethlia01@gmail.com>
2026-02-28 12:45:23 +0100

    test2

--- Assets/CUSTOM/Scripts/Bomb.cs
+++ Assets/CUSTOM/Scripts/Bomb.cs
@@ -3,19 +3,12 @@ using UnityEngine;
 
 public class Bomb : MonoBehaviour
 {
-    private RigidbodySynchronizable _rigid;
 
     [Header("Explosion Settings")]
     public float explosionRadius = 7f;
     public float explosionForce = 1000f;
     public float maxDamage = 50f;
     public GameObject particlePrefab;
-
-    private void Awake()
-    {
-        _rigid = GetComponent<RigidbodySynchronizable>();
-    }
-
 
     private void OnCollisionEnter(Collision collision)
     {
