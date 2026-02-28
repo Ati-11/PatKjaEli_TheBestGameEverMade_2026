bb5544b826f069ebbe5558aab6607916f6705236
Zeth <zethlia01@gmail.com>
2026-02-28 13:36:36 +0100

    Update Launcher.cs

--- Assets/CUSTOM/Scripts/Launcher.cs
+++ Assets/CUSTOM/Scripts/Launcher.cs
@@ -1,10 +1,17 @@
-using UnityEngine;
+using Alteruna;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 
 public class Launcher : MonoBehaviour
 {
     public GameObject BombPrefab;
+    public Alteruna.Avatar avatar;
+
+    private void Awake()
+    {
+        avatar = GetComponent<Alteruna.Avatar>();
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,6 +21,12 @@ public class Launcher : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if (!avatar.IsMe)
+        {
+            return;
+        }
+
         if (Input.GetKeyDown(KeyCode.Q))
         {
             GameObject bomb = Instantiate(BombPrefab, this.transform.position, Quaternion.identity);
