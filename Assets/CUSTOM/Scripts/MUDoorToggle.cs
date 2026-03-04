58cb43c0d5d9a48450c8169e0b11da704db94650
Zeth <zethlia01@gmail.com>
2026-03-04 14:25:53 +0100

    Door sound added, Door sound works for doors /code

--- Assets/CUSTOM/Scripts/MUDoorToggle.cs
+++ Assets/CUSTOM/Scripts/MUDoorToggle.cs
@@ -1,3 +1,4 @@
+using FMODUnity;
 using UnityEngine;
 
 
@@ -7,6 +8,8 @@ public class MUDoorToggle : MonoBehaviour
 
     private Alteruna.AnimationSynchronizable _aniSync;
 
+    [Header("Audio")]
+    [SerializeField] private EventReference doorSound;
 
     public bool isInDoorVolume = false;
     public bool doorIsOpen = false;
@@ -26,7 +29,8 @@ public class MUDoorToggle : MonoBehaviour
                 Debug.Log("E PRESSED");
 
                 doorIsOpen = !doorIsOpen;
-                    
+
+                RuntimeManager.PlayOneShot(doorSound, transform.position);
                 // play for all clients
                 _aniSync.SetBool("Open Right", doorIsOpen);
                 _aniSync.SetBool("Open Left", doorIsOpen);
